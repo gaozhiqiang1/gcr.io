156f470a481aad762c143096e9e4814d577d4a96
solomonlinux <root@localhost.localdomain>
2019-03-14 13:25:17 -0400

    test 13

--- test-v0.5.sh
+++ test-v0.5.sh
@@ -86,7 +86,8 @@ sdk_install(){
 sdk_auth(){
 	local AUTH_COUNT=$(gcloud auth list --format="get(ACCOUNT)" | wc -l)
 	if [ $AUTH_COUNT -eq 0 ]; then
-		gcloud auth activate-service-account --key-file ~/gcloud.config.json
+		#gcloud auth activate-service-account --key-file ~/gcloud.config.json
+		gcloud auth activate-service-account --key-file ~/gcrio-images-6bdc946edf5b.json
 	fi
 }
 
