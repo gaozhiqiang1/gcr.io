a6c445b78768516b1c66ad8ab7dca36836def86b
solomonlinux <root@localhost.localdomain>
2019-03-15 02:42:35 -0400

    test 39

--- test-v0.5.sh
+++ test-v0.5.sh
@@ -139,7 +139,7 @@ image_list_create(){
 				echo ${IMAGE}:${TAG} >> $IMAGE_LIST
 			fi
 			#echo ${IMAGE}:${TAG} >> list.txt &
-			文件行数: $(wc -l $IMAGE_LIST)
+			echo "文件行数: $(wc -l $IMAGE_LIST)"
 		done < <(gcloud container images list-tags $IMAGE --format="get(TAGS)" --filter='tags:*' | sed 's#;#\n#g')
 
 	done < <(gcloud container images list --repository=$NAMESPACE --format="value(NAME)")
